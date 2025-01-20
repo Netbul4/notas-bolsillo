c24a3e88f1991fd316833dde2f661b250b402cc3
Eddie <eddiepp06@gmail.com>
2025-01-20 16:12:15 -0400

    fix: Llamando clase inexistente

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,6 @@
 import { Text, View } from "react-native";
 import Login from "./tabs/login";
 import StudentMain from "./tabs/student-main";
-import StudentSchoolSubject from "./tabs/student-school-subject";
 import SubjectTasks from "./tabs/student-subject-tasks";
 
 export default function Index() {
